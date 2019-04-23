e48d50f5cb06792b6dacb0dabb629c3aacd2363a
Simon Konstantyner <simon@konstantyner.dk>
2019-04-23 10:13:03 +0200

    Added toggle and updated nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,82 +1,90 @@
-const pkg = require('./package')
+const pkg = require('./package');
 
 module.exports = {
-  mode: 'universal',
+    mode: 'universal',
 
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: 'SK Programming',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito:400,700' }
-    ]
-  },
-
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: {
-    color: '#38C172',
-    height: '5px'
-  },
-
-  /*
-   ** Global CSS
-   */
-  css: [
-    '~/assets/css/main.scss'
-  ],
-
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-
-  /*
-   ** Nuxt.js modules
-   */
-  modules: ['@nuxtjs/pwa'],
-
-  /*
-   ** Build configuration
-   */
-  build: {
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+    head: {
+        title: 'SK Programming',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: pkg.description
+            }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css?family=Nunito:400,700'
+            }
+        ]
+    },
+
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: {
+        color: '#38C172',
+        height: '5px'
+    },
+
+    /*
+     ** Global CSS
+     */
+    css: ['~/assets/css/main.scss'],
+
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [{ src: '~/plugins/vue-js-toggle-button', ssr: false }],
+
+    /*
+     ** Nuxt.js modules
+     */
+    modules: ['@nuxtjs/pwa'],
+
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {
+            // Run ESLint on save
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                });
+            }
+        }
+    },
+
+    /*
+     ** Transition Settings
+     */
+    transition: {
+        name: 'page',
+        mode: 'out-in'
+    },
+
+    /*
+     ** Router Settings
+     */
+    router: {
+        linkActiveClass: '',
+        linkExactActiveClass: 'border-b-2'
     }
-  },
-
-  /*
-   ** Transition Settings
-   */
-  transition: {
-    name: 'page',
-    mode: 'out-in'
-  },
-
-  /*
-   ** Router Settings
-   */
-  router: {
-    linkActiveClass: '',
-    linkExactActiveClass: 'border-b-2'
-  },
-}
+};
--- pages/darkmode/index.vue
+++ pages/darkmode/index.vue
@@ -1,14 +1,16 @@
 <template>
-    <div id="darkmode">
-        <div class="container mx-auto my-6">
-            <h1>Welcome to the darkside</h1>
-
-            <span>Darkmode</span>
-            <input
-                type="checkbox"
-                :checked="darkmode"
-                @change="toggleDarkmode()"
-            />
+    <div id="darkmode" class="container mx-auto my-6 text-center">
+        <div class="mx-6 mt-6">
+            <h2 class="text-3xl font-bold mb-6">Enter darkmode?</h2>
+            <p class="text-base text-grey-darker">
+                <no-ssr>
+                    <toggle-button
+                        :value="darkmode"
+                        color="#38C172"
+                        @change="toggleDarkmode()"
+                    />
+                </no-ssr>
+            </p>
         </div>
     </div>
 </template>
