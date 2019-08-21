79d24d9c3c998381aacc42edbd391bcb245d5b3b
Simon Konstantyner <simon@konstantyner.dk>
2019-08-21 18:06:45 +0200

    added svg waves to footer

--- components/layout/AppFooter.vue
+++ components/layout/AppFooter.vue
@@ -1,15 +1,36 @@
 <template>
-    <footer class="container mx-auto px-12 py-6 text-base text-gray-600">
-        <div class="md:flex md:justify-center text-center">
-            <p class="md:mr-6">&copy; SK Programmering - 2019</p>
-            <a
-                href="https://datacvr.virk.dk/data/visenhed?enhedstype=virksomhed&id=40293833"
-                target="_blank"
-                class="hover:underline"
-                >CVR-nr. 40 29 38 33</a
-            >
+    <footer>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+                fill="#38a169"
+                fill-opacity="0.8"
+                d="M0,96L60,90.7C120,85,240,75,360,90.7C480,107,600,149,720,138.7C840,128,960,64,1080,32C1200,0,1320,0,1380,0L1440,0L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+            ></path>
+            <path
+                fill="#38a169"
+                fill-opacity="1"
+                d="M0,64L60,64C120,64,240,64,360,101.3C480,139,600,213,720,218.7C840,224,960,160,1080,117.3C1200,75,1320,53,1380,42.7L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+            ></path>
+            <path
+                fill="#38a169"
+                fill-opacity="0.6"
+                d="M0,192L60,160C120,128,240,64,360,37.3C480,11,600,21,720,69.3C840,117,960,203,1080,202.7C1200,203,1320,117,1380,74.7L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+            ></path>
+        </svg>
+        <div class="bg-green-600">
+            <div class="container mx-auto px-12 pb-6 text-base text-gray-100">
+                <div class="md:flex md:justify-center text-center">
+                    <p class="md:mr-6">&copy; SK Programmering - 2019</p>
+                    <a
+                        href="https://datacvr.virk.dk/data/visenhed?enhedstype=virksomhed&id=40293833"
+                        target="_blank"
+                        class="hover:underline"
+                        >CVR-nr. 40 29 38 33</a
+                    >
+                </div>
+                <div class="flex justify-center pt-3">(っ-◉◡◉)っ</div>
+            </div>
         </div>
-        <div class="flex justify-center pt-3">(っ-◉◡◉)っ</div>
     </footer>
 </template>
 
